b997fff90eeaf86af15f8cc0de0661d661b5a42a
Ludo Ruisch <admin@ludoruisch.nl>
2016-01-23 06:57:13 +0100

    Solved Problem 28

---
+++ Problem 28/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/*
+	What is the sum of the numbers on the diagonals in a 1001 by 1001
+	spiral formed in the same way?
+*/
+
+int sumDiagonals(int x, int y) {
+	int a, b, s, r;
+	
+	s = x * y;
+	a = 1;
+	b = 2;
+	r += a;
+	
+	while(a < s) {
+		r += (a * 4) + (b * 10);
+		a += (b * 4);		
+		b += 2;
+	}
+	
+	return r;
+}
+
+int main() {
+	cout << "result = " << sumDiagonals(1001, 1001) << endl;
+	
+	return 0;
+}
