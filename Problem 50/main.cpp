d29909aff32e66ce129bfc9b9a856ac2660704dd
Ludo Ruisch <admin@ludoruisch.nl>
2016-01-29 14:26:20 +0100

    Solved Problem 50

---
+++ Problem 50/main.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <numeric>
+#include <fstream>
+#include <string.h>
+
+using namespace std;
+
+/*
+	Which prime, below one-million, can be written as the sum
+	of the most consecutive primes?
+*/
+
+static vector<int> primes;
+
+bool isPrime(int v) {
+	vector<int>::iterator it;
+	
+	it = find(primes.begin(), primes.end(), v);
+	if(it != primes.end())
+		return true;
+	
+	return false;
+}
+
+void genPrime(int m) {
+	int i, j;
+	vector<int> a, b;
+	
+	for(i = 2; i <= m * m; i++)
+		a.push_back(i);
+	
+	for(i = 2; i <= m; i++) {
+		for(j = 0; j < a.size(); j++) {
+			if(a[j] % i != 0 || a[j] <= i)
+				b.push_back(a[j]);
+		}
+		a = b;
+		b.clear();
+	}
+	
+	primes = a;
+}
+
+int findMostConsecutivePrimes(int m) {
+	int i, v, l, r, p0, p1;
+	
+	r = 0;		// highest result we generate
+	l = 0;		// length of added primes to generate a valid prime
+	v = 0;		// generated value
+	
+	p0 = 0;		// position of prime we need to subtract
+	p1 = 0;		// position of prime we need to add
+	
+	/*
+		Find the first one in a row added prime
+	*/
+	for(i = 0; i < primes.size() && v + primes[i] < m; i++) {
+		v += primes[i];
+		
+		if(isPrime(v)) {
+			l = i;
+			r = v;
+		}
+		
+		p1 = i;
+	}
+	
+	// try looking for a new prime number that has a higher length of primes
+	while(l < p1 - p0) {
+		v -= primes[p0];
+		p0++;
+		
+		// the generated prime number must not be higher then our max
+		if(v + primes[p1] < m) {
+			v += primes[p1];
+			p1++;
+		}
+		
+		if(isPrime(v)) {
+			l = p1 - p0;
+			r = v;
+		}
+	}
+	
+	return r;
+}
+
+int main() {
+	genPrime(1000);
+	
+	cout << "result = " << findMostConsecutivePrimes2(1000000) << endl;
+	
+	return 0;
+}
