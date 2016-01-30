b762f96d562f55940aacca4d1d716f584ca43d15
Ludo Ruisch <admin@ludoruisch.nl>
2016-01-30 19:19:54 +0100

    Solved Problem 55

---
+++ 51-100/Problem 55/main.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <numeric>
+#include <fstream>
+#include <string.h>
+
+#include "gmp.h"
+
+using namespace std;
+
+/*
+	How many Lychrel numbers are there below ten-thousand?
+*/
+
+bool isPalindrome(string v) {
+	int i;
+	
+	for(i = 0; i < v.size() / 2; i++)
+		if(v[i] != v[v.size() - 1 - i])
+			return false;
+	
+	return true;
+} 
+
+int countLychrelNumbers(int l, int it) {
+	int i, j, c;
+	string s;
+	vector<string> v, t;
+	mpz_t a, b;
+	char buffer[10000];
+	
+	//Init
+	c = 0;
+	mpz_init(a);
+	mpz_init(b);
+	
+	//Add all values in list
+	for(i = 1; i <= l; i++)
+		v.push_back(to_string(i));
+	
+	//Update list and remove values that are Palindrome
+	for(i = 0; i < it; i++) {
+		t.clear();
+		for(j = 0; j < v.size(); j++) {
+			//Reverse text
+			s = string(v[j].rbegin(), v[j].rend());
+			
+			//Set 
+			mpz_set_str(a, v[j].c_str(), 10);
+			mpz_set_str(b, s.c_str(), 10);
+			
+			// a + b
+			mpz_add(a, a, b);
+			
+			//Save result
+			mpz_get_str(buffer, 10, a);
+			v[j] = buffer;
+			
+			//Check if Palindrome
+			if(!isPalindrome(v[j]))
+				t.push_back(v[j]);
+		}
+		v = t;
+	}
+	
+	return v.size();
+}
+
+int main() {
+	cout << "result = " << countLychrelNumbers(10000, 25) << endl;
+	
+	return 0;
+}
