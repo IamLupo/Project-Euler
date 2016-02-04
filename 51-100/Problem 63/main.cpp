3c2aa823b36d4a6b6291fa9723664277a55741d9
Ludo Ruisch <admin@ludoruisch.nl>
2016-02-04 03:52:22 +0100

    Solved Problem 63

---
+++ 51-100/Problem 63/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <numeric>
+#include <fstream>
+#include <string.h>
+
+#include "gmp.h"
+
+using namespace std;
+
+/*
+	How many n-digit positive integers exist which are also an nth power?
+*/
+
+int countNthPower() {
+	int i, j, c;
+	mpz_t v;
+	string s;
+	
+	//Init
+	mpz_init(v);
+	i = 1;
+	j = 1;
+	c = 0;
+	
+	while(j < 30) {
+		// v = i^j
+		mpz_ui_pow_ui(v, i, j);
+		
+		//Convert
+		s = mpz_get_str(NULL, 10, v);
+		
+		if(s.size() == j) {
+			c++;
+			i++;
+		}
+		else if(s.size() > j) {
+			j++;
+			i = 1;
+		}
+		else
+			i++;
+	}
+	
+	return c;
+}
+
+int main() {
+	cout << "result = " << countNthPower() << endl;
+	
+	return 0;
+}
