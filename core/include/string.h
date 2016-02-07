671f9a167632390ddc71e0f867c69fb1cbee4c85
Ludo Ruisch <admin@ludoruisch.nl>
2016-02-08 00:32:36 +0100

    add explode in String

--- core/include/string.h
+++ core/include/string.h
@@ -1,8 +1,12 @@
 #include <string>
+#include <vector>
+#include <sstream>
+#include <utility>
 
 namespace IamLupo {
 	namespace String {
 		long long addIntegers(const std::string &s);
 		bool isPalindrome(const std::string &s);
+		std::vector<std::string> explode(const std::string &s, char delim);
 	}
 }
--- core/src/String.cpp
+++ core/src/String.cpp
@@ -21,4 +21,15 @@ bool IamLupo::String::isPalindrome(const std::string &s) {
 			return false;
 	
 	return true;
-} 
+}
+
+std::vector<std::string>
+IamLupo::String::explode(std::string const & s, char delim) {
+	std::vector<std::string> result;
+	std::istringstream iss(s);
+
+	for (std::string token; std::getline(iss, token, delim);)
+		result.push_back(std::move(token));
+
+	return result;
+}
