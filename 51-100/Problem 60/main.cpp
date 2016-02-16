7ac6cd2948e06c663cd188855574ac31c1fde376
Ludo Ruisch <admin@ludoruisch.nl>
2016-02-16 14:55:34 +0100

    implement library functions

--- 51-100/Problem 60/main.cpp
+++ 51-100/Problem 60/main.cpp
@@ -8,6 +8,9 @@
 
 #include "gmp.h"
 
+#include "prime.h"
+#include "number.h"
+
 using namespace std;
 
 /*
@@ -15,98 +18,7 @@ using namespace std;
 	two primes concatenate to produce another prime.
 */
 
-static vector<int> primes;
-
-bool isPrime2(long long v) {
-	int i;
-	
-	for(i = 0; i < primes.size() && primes[i] < v / 2; i++) {
-		if(v % primes[i] == 0)
-			return false;
-	}
-	
-	return true;
-}
-
-bool isPrime(int v) {
-	vector<int>::iterator it;
-	
-	it = find(primes.begin(), primes.end(), v);
-	if(it != primes.end())
-		return true;
-	
-	return false;
-}
-
-void genPrime(int m) {
-	int i, j;
-	vector<int> a, b;
-	
-	for(i = 2; i <= m * m; i++)
-		a.push_back(i);
-	
-	for(i = 2; i <= m; i++) {
-		for(j = 0; j < a.size(); j++) {
-			if(a[j] % i != 0 || a[j] <= i)
-				b.push_back(a[j]);
-		}
-		a = b;
-		b.clear();
-	}
-	
-	primes = a;
-}
-
-int sum(vector<int> v) {
-	int i, s;
-	
-	s = 0;
-	for(i = 0; i < v.size(); i++)
-		s += v[i];
-	
-	return s;
-}
-
-/*
-	It will give the next base number of a value
-
-	Example:
-		f(5) = 10
-		f(9) = 10
-		f(11) = 100
-		f(55) = 100
-		f(101) = 1000
-*/
-int nextBase10(int v) {
-	int n;
-
-	n = 1;
-	
-	while((v / n) != 0)
-		n *= 10;
-	
-	return n;
-}
-
-/*
-	Example:
-		f({3}, 5) = false, because 35 isnt prime
-		f({3, 7}, 13) = false, because 133 and 713 isnt a prime
-		f({3, 7}, 109) = true, because 3109, 1093, 1097 and 7109 are prime
-*/
-bool isConcatenatePrime(const vector<int> &r, int v) {
-	int i;
-	
-	for(i = 0; i < r.size(); i++) {
-		if(!isPrime2((v * nextBase10(r[i])) + r[i]))
-			return false;
-		
-		if(!isPrime2((r[i] * nextBase10(v)) + v))
-			return false;
-	}
-	
-	return true;
-}
+static IamLupo::Primes primes;
 
 void brutforce(vector<int> &r, int* m, int index, int max) {
 	int s;
@@ -114,12 +26,12 @@ void brutforce(vector<int> &r, int* m, int index, int max) {
 	
 	if(r.size() == max) {
 		//Calculate the new max value
-		*m = sum(r);
+		*m = IamLupo::Number::sum(r);
 		return;
 	}
 	
 	//Calculate the sum of the valide values
-	s = sum(r);
+	s = IamLupo::Number::sum(r);
 	
 	/*
 		rules:
@@ -131,9 +43,9 @@ void brutforce(vector<int> &r, int* m, int index, int max) {
 			(*m < s + primes[index] && primes[index] != 0)) {
 		
 		/*
-			check if index prime van be fit with the already found primes list
+			check if index prime will be fit with the already found primes list
 		*/
-		if(isConcatenatePrime(r, primes[index])) {
+		if(IamLupo::Prime::isConcatenate(primes, r, primes[index])) {
 			//Copy already valid primes
 			t = r;
 			
@@ -160,7 +72,7 @@ int sumConcatenatePrimes(int l) {
 }
 
 int main() {
-	genPrime(100);
+	IamLupo::Prime::generate(primes, 100);
 	
 	cout << "result = " << sumConcatenatePrimes(5) << endl;
 	
