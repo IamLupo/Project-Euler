b47760734be274a92c51d2ccca6636e89ff1ce52
Ludo Ruisch <admin@ludoruisch.nl>
2016-03-07 20:13:48 +0100

    update: Problem 14

---
+++ 1-50/Problem 14/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <numeric>
+#include <fstream>
+#include <string.h>
+#include <stdlib.h>
+
+using namespace std;
+
+/*
+	Which starting number, under one million, produces the longest chain?
+*/
+
+int getLongestChain(int m) {
+	int i, c, h, r;
+	long long value;
+	
+	//Init
+	h = 0;
+	r = 0;
+	
+	for(i = 1; i < m; i++) {
+		value = i;
+		c = 0;
+		
+		while(value != 1) {
+			if(value % 2 == 0)
+				value /= 2;
+			else
+				value = (value * 3) + 1;
+			c++;
+		}
+		
+		if(h < c) {
+			h = c;
+			r = i;
+		}
+	}
+	
+	return r;
+}
+
+int main() {
+	cout << "result = " << getLongestChain(1000000) << endl;
+	
+	return 0;
+}
