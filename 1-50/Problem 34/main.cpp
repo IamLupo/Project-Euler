687a7a2e1325b5c4bb60fa21c0a9b6459144bb20
Ludo Ruisch <admin@ludoruisch.nl>
2016-03-10 17:10:49 +0100

    update: Problem 34

--- 1-50/Problem 34/main.cpp
+++ 1-50/Problem 34/main.cpp
@@ -3,6 +3,9 @@
 #include <algorithm>
 #include <math.h>
 #include <numeric>
+#include <fstream>
+#include <string.h>
+#include <stdlib.h>
 
 using namespace std;
 
@@ -10,18 +13,19 @@ using namespace std;
 	Find the sum of all numbers which are equal to the sum of the factorial of their digits.
 */
 
-static vector<int> options = { 1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880 };
-
-int sumFractorial() {
+int sumCurious() {
 	int i, j, v, r;
+	vector<int> o;
 
+	//Init
 	r = 0;
+	o = {1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880};
 	
-	for(i = 10; i < 50000; i++) {
+	for(i = 10; i < 41000; i++) {
 		v = 0;
 		
 		for(j = i; j > 0; j /= 10)
-			v += options[j % 10];
+			v += o[j % 10];
 		
 		if(v == i)
 			r += v;
@@ -31,7 +35,7 @@ int sumFractorial() {
 }
 
 int main() {
-	cout << "result = " << sumFractorial() << endl;
+	cout << "result = " << sumCurious() << endl;
 	
 	return 0;
 }
