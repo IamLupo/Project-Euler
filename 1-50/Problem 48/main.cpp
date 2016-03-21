de7acf71d283ab32b91268fe16e943c8f4d27e84
Ludo Ruisch <admin@ludoruisch.nl>
2016-03-21 09:43:32 +0100

    update: Problem 48

--- 1-50/Problem 48/main.cpp
+++ 1-50/Problem 48/main.cpp
@@ -5,6 +5,8 @@
 #include <numeric>
 #include <fstream>
 #include <string.h>
+#include <stdlib.h>
+
 #include "gmp.h"
 
 using namespace std;
@@ -13,25 +15,27 @@ using namespace std;
 	Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
 */
 
-string findSelfPowers(int r) {
+string findSelfPowers(int l) {
 	int i;
-	char buffer[10000];
 	string s;
 	mpz_t t, v;
 	
+	//Init
 	mpz_init(t);
 	mpz_init_set_ui(v, 0);
 	
-	for(i = 1; i <= r; i++) {
+	for(i = 1; i <= l; i++) {
+		//v += i^i
 		mpz_set_ui(t, i);
 		mpz_pow_ui(t, t, i);
 		mpz_add(v, v, t);
 	}
 	
-	mpz_get_str(buffer, 10, v);
-	s.insert(s.begin(), buffer + strlen(buffer) - 10, buffer + strlen(buffer));
+	//Convert
+	s = mpz_get_str(nullptr, 10, v);
 	
-	return s;
+	//return the last 10 charters
+	return s.substr(s.size() - 10, 10);
 }
 
 int main() {
