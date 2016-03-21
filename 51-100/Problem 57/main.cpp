ff1182931e78e56b7e65129f2a3a0b11a76a66a7
Ludo Ruisch <admin@ludoruisch.nl>
2016-03-21 11:47:59 +0100

    update: Problem 57

--- 51-100/Problem 57/main.cpp
+++ 51-100/Problem 57/main.cpp
@@ -5,6 +5,7 @@
 #include <numeric>
 #include <fstream>
 #include <string.h>
+#include <stdlib.h>
 
 #include "gmp.h"
 
@@ -16,17 +17,16 @@ using namespace std;
 */
 
 int countBiggerNumeratorDigit() {
-	int i;
-	long long c;
+	int i, r;
 	mpz_t x, y, d, a;
-	char c_x[10000], c_y[10000];
+	string s, t;
 	
 	//Init
+	r = 0;
 	mpz_init_set_ui(x, 3);
 	mpz_init_set_ui(y, 2);
 	mpz_init_set_ui(d, 1);
 	mpz_init(a);
-	c = 0;
 	
 	for(i = 0; i < 1000; i++) {
 		//a = y
@@ -45,14 +45,14 @@ int countBiggerNumeratorDigit() {
 		mpz_set(d, a);
 		
 		//Get length of number
-		mpz_get_str(c_x, 10, x);
-		mpz_get_str(c_y, 10, y);
+		s = mpz_get_str(nullptr, 10, x);
+		t = mpz_get_str(nullptr, 10, y);
 		
-		if(strlen(c_x) > strlen(c_y))
-			c++;
+		if(s.size() > t.size())
+			r++;
 	}
 	
-	return c;
+	return r;
 }
 
 int main() {
