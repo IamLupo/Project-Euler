16093adf647df907c906d0e7cf5322d4fe6caa01
Ludo Ruisch <admin@ludoruisch.nl>
2016-03-31 01:19:48 +0200

    update: Problem 68

--- 51-100/Problem 68/main.cpp
+++ 51-100/Problem 68/main.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <algorithm>
 #include <math.h>
+#include <numeric>
+#include <fstream>
 #include <string.h>
 #include <stdlib.h>
 
@@ -16,6 +18,10 @@ typedef vector<int> GonRingResults;
 
 static vector<GonRingResults> grr_list;
 
+/*
+	Example:
+		f({1,2,3,4,5,6}) = {2, 1, 3, 4, 3, 5, 6, 5, 1}
+*/
 GonRingResults convert(GonRing &gr) {
 	int i;
 	vector<int> r;
@@ -33,6 +39,10 @@ GonRingResults convert(GonRing &gr) {
 	return r;
 }
 
+/*
+	Example:
+		f({2, 1, 3, 4, 3, 5, 6, 5, 1}) = true, because 2 > 4 and 2 > 6
+*/
 bool check(GonRingResults &grr) {
 	int i, j, v;
 	bool f;
@@ -49,36 +59,46 @@ bool check(GonRingResults &grr) {
 
 void bruteforce(GonRing &gr, int s, int l, int m) {
 	int i, v;
-	GonRing temp_gr;
-	GonRingResults grr;
-	vector<int>::iterator a, b;
 	
+	//Max level reached
 	if(l == m - 1) {
-		v = s - gr[0] - gr[l * 2];
-		a = find(gr.begin(), gr.end(), v);
+		GonRingResults grr;
 		
-		if(v > 0 && v <= m * 2 && a == gr.end()) {
+		//Generate last value
+		v = s - gr[0] - gr[l * 2];
+		
+		//Check if last value is in range and does not exist in the gonring yet
+		if(v > 0 && v <= m * 2 && find(gr.begin(), gr.end(), v) == gr.end()) {
 			gr.push_back(v);
+			
+			//Convert GonRing to GonRing Result
 			grr = convert(gr);
 			
-			if(check(grr)) {
+			//Check if GonRing Result has the right structure
+			if(check(grr))
 				grr_list.push_back(grr);
-			}
 		}
 		
 		return;
 	}
 	
+	//Generate new GonRings
 	for(i = 1; i <= m * 2; i++) {
 		v = s - gr[l * 2] - i;
-		a = find(gr.begin(), gr.end(), i);
-		b = find(gr.begin(), gr.end(), v);
 		
-		if(v > 0 && v <= m * 2 && i != v && a == gr.end() && b == gr.end()) {
+		//Check if the two values are in range and does not exist in the gonring yet
+		if(	v > 0 && v <= m * 2 && i != v &&
+			find(gr.begin(), gr.end(), i) == gr.end() &&
+			find(gr.begin(), gr.end(), v) == gr.end()) {
+			
+			GonRing temp_gr;
+			
+			//Generate a new gonring
 			temp_gr = gr;
 			temp_gr.push_back(i);
 			temp_gr.push_back(v);
 			
+			//bruteforce next level
 			bruteforce(temp_gr, s, l + 1, m);
 		}
 	}
@@ -86,10 +106,11 @@ void bruteforce(GonRing &gr, int s, int l, int m) {
 
 long long getMaximumDigits(int m, int d) {
 	int i, j, l, s;
-	long long v, h;
+	long long v, r;
 	GonRing gr;
 	
-	h = 0;
+	//Init
+	r = 0;
 	
 	//Get all possible Gon Rings
 	for(i = 1; i <= (m * 6) - 3; i++) {
@@ -99,12 +120,13 @@ long long getMaximumDigits(int m, int d) {
 		}
 	}
 	
-	//Find the one with 16 digit and highest value
+	//Find the one with {d} digit and highest value
 	for(i = 0; i < grr_list.size(); i++) {
 		v = 0;
 		l = 0;
 		s = grr_list[i].size() - 1;
 		
+		//Convert GonRing Result to Integer
 		for(j = 0; j < s + 1; j++) {
 			v += grr_list[i][s - j] * pow(10, l);
 			
@@ -114,14 +136,17 @@ long long getMaximumDigits(int m, int d) {
 				l++;
 		}
 		
-		if(v < pow(10, d) && v > h)
-			h = v;
+		if(v < pow(10, d) && v > r)
+			r = v;
 	}
 	
-	return h;
+	return r;
 }
 
 int main() {
+	//Example
+	//cout << "result = " << getMaximumDigits(3, 9) << endl;
+	
 	cout << "result = " << getMaximumDigits(5, 16) << endl;
 	
 	return 0;
