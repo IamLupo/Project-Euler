ae01b7f1ff00e81e3a10f043eb4fc520f2db49c1
Ludo Ruisch <admin@ludoruisch.nl>
2016-04-14 20:12:21 +0200

    update: Problem 62

--- 51-100/Problem 62/main.cpp
+++ 51-100/Problem 62/main.cpp
@@ -15,62 +15,38 @@ using namespace std;
 	Find the smallest cube for which exactly five permutations of its digits are cube.
 */
 
-int countOptions(const string &s) {
-	int i, c;
-	mpz_t t, min, max;
-	string r;
+string findSmallestCube(int l) {
+	int i;
+	mpz_t v;
+	string s;
+	vector<string> r;
 	
 	//Init
-	mpz_init_set_ui(t, 0);
-	mpz_init(min);
-	mpz_init(max);
-	c = 0;
+	mpz_init(v);
 	
-	//Calculate Limits
-	mpz_ui_pow_ui(min, 10, s.size() - 1);
-	mpz_ui_pow_ui(max, 10, s.size());
-	
-	for(i = 2; mpz_cmp(t, max) < 0; i++) {
-		mpz_ui_pow_ui(t, i, 3);
+	for(i = 1; i <= 10000; i++) {
+		mpz_ui_pow_ui(v, i, 3);
 		
-		if(mpz_cmp(t, min) >= 0) {
-			// Convert and Sort
-			r = mpz_get_str(NULL, 10, t);
-			sort(r.begin(), r.end());
+		s = mpz_get_str(NULL, 10, v);
+		sort(s.begin(), s.end());
+		
+		r.push_back(s);
+	}
+	
+	for(i = 0; i < r.size(); i++) {
+		if(count(r.begin(), r.end(), r[i]) >= l) {
+			mpz_ui_pow_ui(v, i + 1, 3);
 			
-			if(s.compare(r) == 0)
-				c++;
+			return mpz_get_str(NULL, 10, v);
 		}
 	}
 	
-	return c;
-}
-
-string findSmallestCube(int l) {
-	int i;
-	mpz_t t;
-	string v;
-	
-	//Init
-	mpz_init(t);
-	
-	for(i = 2; i < 1000000; i++) {
-		mpz_ui_pow_ui(t, i, 3);
-		
-		// Convert and Sort
-		v = mpz_get_str(NULL, 10, t);
-		sort(v.begin(), v.end());
-	
-		if(countOptions(v) >= l)
-			return mpz_get_str(NULL, 10, t);
-	}
-	
-	v.clear();
-	
-	return v;
+	return "";
 }
 
 int main() {
+	//cout << "result = " << findSmallestCube(3) << endl;
+	
 	cout << "result = " << findSmallestCube(5) << endl;
 	
 	return 0;
