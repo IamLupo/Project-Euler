391e38c0c9b39fd91f4544d1aa85361545e27398
Ludo Ruisch <admin@ludoruisch.nl>
2016-04-29 22:18:37 +0200

    update: Problem 73 speed increasement

--- 51-100/Problem 73/main.cpp
+++ 51-100/Problem 73/main.cpp
@@ -4,61 +4,14 @@
 #include <math.h>
 #include <string.h>
 #include <stdlib.h>
-
+ 
 using namespace std;
-
+ 
 /*
 	How many fractions lie between 1/3 and 1/2 in the sorted set of
-	reduced proper fractions for d ≤ 12,000?
+	reduced proper fractions for d = 12,000?
 */
-
-vector<vector<int>> dividers(int l) {
-	int i, j;
-	vector<vector<int>> r;
-	
-	//zero has no dividers
-	r.push_back({0});
-	
-	//All the others have 1 as divider
-	for(i = 1; i <= l; i++)
-		r.push_back({1});
-	
-	//Find the other dividers
-	for(i = 2; i <= l / 2; i++)
-		for(j = 2; i * j <= l; j++)
-			r[i * j].push_back(i);
-	
-	return r;
-}
-
-/*
-	Data:
-		1, -1, -1, 0, -1, 1, -1, 0, 0, 1, -1, 0, -1, 1, 1, 0, -1, 0,
-		-1, 0, 1, 1, -1, 0, 0, 1, 0, 0, -1, -1, -1, 0, 1, 1, 1, 0, -1,
-		1, 1, 0, -1, -1, -1, 0, 0, 1, -1, 0, 0, 0, 1, 0, -1, 0, 1, 0, 
-
-	Property:
-		μ(n) = 1 if n is a square-free positive integer with an even number of prime factors.
-		μ(n) = −1 if n is a square-free positive integer with an odd number of prime factors.
-		μ(n) = 0 if n has a squared prime factor.
-	
-	Reference:
-		https://oeis.org/A008683
-		https://en.wikipedia.org/wiki/M%C3%B6bius_function
-*/
-vector<int> Mobius(long long n) {
-	int i, j;
-	vector<int> r(n + 1, 0);
-	
-	r[1] = 1;
-
-	for(i = 1; i <= n; i++)
-		for(j = 2 * i; j <= n; j += i)
-			r[j] -= r[i];
-	
-	return r;
-}
-
+ 
 /*
 	Data:
 		1, 0, 1, 1, 1, 1, 2, 1, 2, 2, 2, 2, 3, 2, 3, 3, 3, 3, 4, 3, 4, 4,
@@ -79,57 +32,33 @@ long long A103221(long long n) {
 	else
 		return (n / 6) + 1;
 }
-
-/*
-	Data:
-		0,  1,  0,  0,  1,  0,  1,  1,  1,  0,  2,  1,  2,  1,  1,  1,  3,  1,
-		3,  2,  2,  1,  4,  1,  3,  2,  3,  2,  5,  2,  5,  3,  3,  2,  4,  2,
-		6,  3,  4   2,  7,  2,  7,  4,  4,  3,  8,  3,  7,  4,  5,  4,  9,  3,
-		6,  4,  6,  4,  10, 2,  10, 5,  6,  5,  8,  4,  11, 6,  7,  4,  12, 4,
-		12, 6,  7,  6,  10, 4,  13, 6,  9,  6,  14, 4,  10, 7,  9,  6,  15, 4,
-		12, 8,  10, 7,  12, 5,  16, 7
-	
-	Formula:
-		SUM_{d|n} mu(d) * A103221(n/d), where mu is Mobius function (A008683).
-	
-		Andrew Baxter, Jun 06 2008
-	
-	Reference:
-		https://oeis.org/A128115
-*/
-long long A128115(long long n, vector<int> &mu, vector<int> &div) {
-	int i;
-	long long r;
-	
-	//Init
-	r = 0;
-	
-	if(n == 3)
-		return 0;
-	
-	for(i = 0; i < div.size(); i++)
-		r += mu[div[i]] * A103221(n / div[i]);
-	
-	return r;
-}
-
+ 
 long long countProperFractions(int l) {
-	int i;
+	int i, j;
 	long long r;
-	vector<int> mu;
-	vector<vector<int>> div;
+	vector<int> mu(l + 1, 0);
 	
 	//Init
 	r = 0;
-	div = dividers(l);
-	mu = Mobius(l);
+	mu[1] = 1;
 	
-	for(i = 3; i <= l; i++)
-		r += A128115(i, mu, div[i]);
-	
+	for(i = 1; i <= l; i++) {
+		//Generate Mobius
+		for(j = 2 * i; j <= l; j += i)
+			mu[j] -= mu[i];
+		
+		//Calculate Mobius inverse values
+		if(i >= 2)
+			for(j = 2; i * j <= l; j++)
+				r += mu[i] * A103221(j);
+		
+		if(i >= 4)
+			r += mu[1] * A103221(i);
+	}
+		
 	return r;
 }
-
+ 
 int main() {
 	cout << "result = " << countProperFractions(12000) << endl;
 	
