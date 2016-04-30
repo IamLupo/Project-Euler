3c4469260e8d923e229bdccc1e7d42d12b9d3a18
Ludo Ruisch <admin@ludoruisch.nl>
2016-04-30 22:47:02 +0200

    update: Problem 73

--- 51-100/Problem 73/main.cpp
+++ 51-100/Problem 73/main.cpp
@@ -9,7 +9,7 @@ using namespace std;
  
 /*
 	How many fractions lie between 1/3 and 1/2 in the sorted set of
-	reduced proper fractions for d = 12,000?
+	reduced proper fractions for d <= 12,000?
 */
  
 /*
