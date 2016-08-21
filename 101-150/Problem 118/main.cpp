8c9a1b98186c8b5fe754fd0b616b9e46639a3da0
Ludo Ruisch <admin@ludoruisch.nl>
2016-08-21 16:06:07 +0200

    update: Performance Problem 118

--- 101-150/Problem 118/main.cpp
+++ 101-150/Problem 118/main.cpp
@@ -6,7 +6,6 @@
 #include <fstream>
 #include <string.h>
 #include <stdlib.h>
-#include <set>
 
 #include "IamLupo/number.h"
 #include "IamLupo/prime.h"
@@ -20,30 +19,26 @@ using namespace std;
 
 static IamLupo::Primes primes;
 
-int find(const vector<int> &n, int p) {
-	int i, r, v;
-	vector<int> l;
+int find(int n, int p) {
+	int r, v;
 	
 	//Init
 	r = 0;
+	v = 0;
 	
-	for(i = n.size() - 1; i >= 0; i--) {
-		l = n;
-		l.erase(l.begin(), l.begin() + i);
-		
+	while(n > 0) {
 		//Generate number
-		v = IamLupo::Number::to(l);
+		v *= 10;
+		v += n % 10;
+		n /= 10;
 		
-		if(IamLupo::Prime::is(primes, v) && v > p) {
-			//Remove numbers that has been used to generate prime number
-			vector<int> n2(n.begin(), n.begin() + i);
-			
+		if(v > p && IamLupo::Prime::is(primes, v)) {
 			//No numbers to generate primes
-			if(n2.size() == 0)
+			if(n == 0)
 				r++; //Found a solution
 			else
 				//Count all next level possebilities
-				r += find(n2, v);
+				r += find(n, v);
 		}
 	}
 	
@@ -51,18 +46,21 @@ int find(const vector<int> &n, int p) {
 }
 
 int f() {
-	int r;
-	vector<int> v;
+	int r, v;
+	vector<int> l;
 	
 	//Init
 	r = 0;
-	v = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+	l = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 	
 	//Generate all possible Pandigital numbers
 	do {
+		//Convert to integer
+		v = IamLupo::Number::to(l);
+		
 		//Find all possible Pandigital prime sets 
 		r += find(v, 0);
-	} while(next_permutation(v.begin(), v.end()));
+	} while(next_permutation(l.begin(), l.end()));
 	
 	return r;
 }
